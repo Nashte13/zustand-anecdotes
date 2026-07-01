ce3d9d03f4e86769af471d9d1c741b0ad7379c2b
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-07-01 13:01:12 +0300

    making updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import { useAnecdoteStore } from './store'
 
 const App = () => {
+  const {anecdotes, votes, actions} = useAnecdoteStore()
+
+  //initialize votes if empty
+  if (votes.length === 0) {
+    actions.initiVotes(anecdotes.length)
+  }
 
   return (
     <div>
